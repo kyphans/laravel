95f150d39d6868b93cbb4b1afcecee5074b7813a
Phan Quoc Ky <kyphanz1@gmail.com>
2020-06-26 11:00:09 +0700

    upload files #1

---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('hello',function (){
+    return 'Xin chao ky phan';
+});
+Route::get('testcode', function (){
+    echo "<h1> Xin chao </h1>";
+});
+
+Route::get('database',function (){
+//    Schema::create('users1', function($table)
+//    {
+//        $table->increments('id');
+//    });
+
+//    Schema::drop('users1');
+//    echo "Done!";
+});
+
